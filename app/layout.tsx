7e1a98b586699240d8679ddac6d9b9150ac8dfa8
thbueno <thiagobueno.s@gmail.com>
2025-02-06 18:42:32 +0700

    Add Convex and Clerk integration with initial configuration and middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import ConvexClientProvider from "@/components/ConvexClientProvider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -23,12 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ConvexClientProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ConvexClientProvider>
   );
 }
