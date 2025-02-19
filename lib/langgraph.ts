93da663a09e29d0ff44f4185f1c657057aaa8a6e
thbueno <thiagobueno.s@gmail.com>
2025-02-19 23:33:58 +0700

    Add LangChain model initialization and update package dependencies

---
+++ lib/langgraph.ts
@@ -0,0 +1,14 @@
+import { OpenAI } from "@langchain/openai";
+
+const initialiseModel = () => {
+  const model = new OpenAI({
+    model: "gpt-4o-mini",
+    temperature: 0,
+    maxTokens: undefined,
+    timeout: undefined,
+    maxRetries: 2,
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  return model;
+};
