d45aaa54c6ed70833d92d7b32429934229b06a51
thbueno <thiagobueno.s@gmail.com>
2025-02-21 16:14:37 +0700

    Integrate wxflows SDK for enhanced tool management and add GraphQL schemas for Wikipedia and Google Books APIs

--- lib/langgraph.ts
+++ lib/langgraph.ts
@@ -1,15 +1,50 @@
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 import { OpenAI } from "@langchain/openai";
+import wxflows from "@wxflows/sdk/langchain";
+// Connect to wxflows
+const toolClient = new wxflows({
+  endpoint: process.env.WXFLOWS_ENDPOINT || "",
+  apikey: process.env.WXFLOWS_APIKEY,
+});
+
+// Retrieve the tools
+const tools = await toolClient.lcTools;
+const toolNode = new ToolNode(tools);
 
 const initialiseModel = () => {
   const model = new OpenAI({
     model: "gpt-4o-mini",
-    temperature: 0,
-    maxTokens: undefined,
+    temperature: 0.7,
+    maxTokens: 4096,
     timeout: undefined,
     maxRetries: 2,
     streaming: true,
     apiKey: process.env.OPENAI_API_KEY,
-  });
+    callbacks: [
+      {
+        handleLLMStart: async () => {
+          // console.log("🤖 Starting LLM call");
+        },
+        handleLLMEnd: async (output) => {
+          console.log("🤖 End LLM call", output);
+          const usage = output.llmOutput?.usage;
+          if (usage) {
+            // console.log("📊 Token Usage:", {
+            //   input_tokens: usage.input_tokens,
+            //   output_tokens: usage.output_tokens,
+            //   total_tokens: usage.input_tokens + usage.output_tokens,
+            //   cache_creation_input_tokens:
+            //     usage.cache_creation_input_tokens || 0,
+            //   cache_read_input_tokens: usage.cache_read_input_tokens || 0,
+            // });
+          }
+        },
+        // handleLLMNewToken: async (token: string) => {
+        //   // console.log("🔤 New token:", token);
+        // },
+      },
+    ],
+  }).bindTools(tools);
 
   return model;
 };
